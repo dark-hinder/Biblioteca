32d6316f67a592d078d4f4562a93534965bd8f13
Joao Pedro <joaopedrorosademaria@gmail.com>
2026-05-25 09:58:51 -0300

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -2,19 +2,24 @@ from django.contrib import admin
 from .models import *
 from django.contrib import admin
 
-admin.site.register(Local)
-admin.site.register(Severidade)
-admin.site.register(DadosSatelites)
-admin.site.register(AcaoCausadora)
-admin.site.register(Biodiversidade)
-admin.site.register(PerdaHabitat)
-admin.site.register(EspeciesRisco)
-admin.site.register(Especies)
-admin.site.register(Queimada)
-admin.site.register(Relatorio)
-admin.site.register(Cidades)
-admin.site.register(PoliciaAmbiental)
-admin.site.register(Investimento)
-admin.site.register(Acoes)
-admin.site.register(ClimaTempo)
-admin.site.register(Historico)
+admin.site.register(Cidade)
+admin.site.register(Editora)
+admin.site.register(Leitor)
+admin.site.register(Livro)
+admin.site.register(Genero)
+admin.site.register(Emprestimo)
+# Register your models here.
+
+class LivroInline(admin.TabularInline):
+    model = Livro
+    extra = 1  # Número de livros adicionais para adicionar no admin
+
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)  # Campos que serão exibidos na listagem
+    search_fields = ('nome',)  # Campos que serão pesquisados
+
+    inlines = [LivroInline]  # Adiciona a tabela de livros no admin de gêneros
+
+
+admin.site.register(Autor, AutorAdmin)
